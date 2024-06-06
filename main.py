db2bc40d7c879355cb8ab13dd13c555a8207284c
Ivan Matveev <matveev92iv@icloud.com>
2024-06-06 13:34:02 +0300

    update main.py file to set new route for users routers

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 import logging
 
+from app.api.v1.user_router import router as user_router
+
 
 app = FastAPI(
     debug=True,
     title="Документация API для тестового задания.",
     version="0.0.1",
-    openapi_url="/"
+    docs_url="/",
+    root_path="/api/v1",
+    description="Докуменатция составлена в рамках задания. Для связи: pozitif0898@gmail.com, +79814063284"
 )
+
+app.include_router(router=user_router)
 
 logging.basicConfig(level=logging.INFO)
